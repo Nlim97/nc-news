80d49b7357c1c1a4adb7772587d02b3f7daf020a
Nlim97 <nobellimbu1@gmail.com>
2024-05-10 10:29:16 +0100

    task 8 post comment completed

---
+++ src/components/Form.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { postComment } from "../utils/api";
+
+function CommentForm({article_id}){
+    const [username, setUsername] = useState("")
+    const [body, setBody] = useState("")
+    const [postErr, setPostErr] = useState(null)
+    const [submitted, setSubmitted] = useState(false)
+
+    function handleSubmit(event){
+        event.preventDefault()
+        postComment(article_id,{username: username, body: body}).then((res) => {
+            setBody("")
+            setUsername("")
+            setSubmitted(true) 
+        }).catch((err) => {
+            setPostErr(err)
+        })
+        
+    }
+    if(postErr){
+        return <h3>Error: {postErr.message}</h3>
+    }
+    if(submitted){
+        return <h3>Success!</h3>
+    }
+    return (
+        <form className="commentForm"onSubmit={handleSubmit}>
+        <label htmlFor="username">username</label>
+        <input id="username" onChange={(event) => {
+            setUsername(event.target.value)
+        }} value={username} disabled={submitted}/>
+        <label htmlFor="body">body</label>
+        <textarea id="body" onChange={(event) => {
+            setBody(event.target.value)
+        }} value={body} disabled={submitted}/>
+        <button>Submit</button>
+    </form>
+    )
+}
+
+export default CommentForm;
